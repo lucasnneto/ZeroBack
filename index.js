54f7424fdc7c028fd4b9c1010622c144e601e424
lucasnneto <61164638+lucasnneto@users.noreply.github.com>
2021-08-20 14:53:35 -0300

    get ip

--- index.js
+++ index.js
@@ -210,6 +210,13 @@ io.on("connection", (client) => {
 app.get("/teste", (req, res) => {
   res.send(new Date());
 });
+app.get("/ip", (req, res) => {
+  var remoteIp = (req.headers['x-forwarded-for'] || '').split(',').pop() || // Recupera o IP de origem, caso a fonte esteja utilizando proxy
+                 req.connection.remoteAddress || // Recupera o endereço remoto da chamada
+                 req.socket.remoteAddress || // Recupera o endereço através do socket TCP
+                 req.connection.socket.remoteAddress // Recupera o endereço através do socket da conexão
+  res.send(remoteIp);
+});
 http.listen(port, function () {
   console.log("Zero:" + port);
 });
